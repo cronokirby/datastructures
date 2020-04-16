c5e4f7376ee97eb38b9d9329c782f2fb74054428
Lúcás Meier <cronokirby@gmail.com>
2020-04-16 14:14:19 +0200

    Clean up DrawGraph tests

--- src/DrawGraph.test.ts
+++ src/DrawGraph.test.ts
@@ -1,28 +1,17 @@
-import DrawGraph, { ID, Node } from './DrawGraph';
-import fc from 'fast-check';
+import DrawGraph from './DrawGraph';
+import ID from './ID';
 
 test('basic push works', () => {
+  const id = ID.initial();
   const nodes = [
-    { id: '0' as ID, label: 'Foo' },
-    { id: '1' as ID, label: 'Bar' },
+    { id, label: 'Foo' },
+    { id: id.next(), label: 'Bar' },
   ];
   expect(DrawGraph.empty().push(...nodes)).toEqual(DrawGraph.of(...nodes));
 });
 
 test('push does not modify', () => {
   const empty = DrawGraph.empty();
-  empty.push({ id: '0' as ID, label: 'Bar' });
+  empty.push({ id: ID.initial(), label: 'Bar' });
   expect(empty).toEqual(DrawGraph.empty());
-});
-
-const node: fc.Arbitrary<Node> = fc
-  .tuple(fc.string(), fc.string())
-  .map(([id, label]) => ({ id: id as ID, label }));
-
-test('pushing nodes to empty is the same as calling .of', () => {
-  fc.assert(
-    fc.property(fc.array(node), (nodes) => {
-      expect(DrawGraph.empty().push(...nodes)).toEqual(DrawGraph.of(...nodes));
-    }),
-  );
 });
