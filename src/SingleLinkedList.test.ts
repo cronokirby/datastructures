f95821af8ddc727f9060881063da95dfe0748497
Lúcás Meier <cronokirby@gmail.com>
2020-04-16 13:55:21 +0200

    Add SingleLinkedList data structure

--- src/SingleLinkedList.test.ts
+++ src/SingleLinkedList.test.ts
@@ -1,5 +1,62 @@
-import { add } from './SingleLinkedList';
+import SingleLinkedList, { EMPTY_NODE } from './SingleLinkedList';
+import fc from 'fast-check';
 
-test('add works', () => {
-  expect(add(2, 2)).toEqual(4);
+test('the tail of an empty list is empty', () => {
+  expect(SingleLinkedList.of().tail).toEqual(SingleLinkedList.of());
+});
+
+test('basic appending works', () => {
+  expect(SingleLinkedList.of().append(1).append(2)).toEqual(
+    SingleLinkedList.of(1, 2),
+  );
+});
+
+const nonEmptyValues: fc.Arbitrary<string[]> = fc.array(
+  fc.asciiString(),
+  1,
+  20,
+);
+
+test('prepending works the same as with arrays', () => {
+  fc.assert(
+    fc.property(nonEmptyValues, ([v, ...vs]) => {
+      expect(SingleLinkedList.of(...vs).prepend(v)).toEqual(
+        SingleLinkedList.of(v, ...vs),
+      );
+    }),
+  );
+});
+
+test('prepending works the same as with arrays - test case #1', () => {
+  const [v, ...vs] = ['a'];
+  expect(SingleLinkedList.of(...vs).prepend(v)).toEqual(
+    SingleLinkedList.of(v, ...vs),
+  );
+});
+
+test('appending works the same as with arrays', () => {
+  fc.assert(
+    fc.property(nonEmptyValues, (vs) => {
+      // These two are safe because we generate non empty values
+      const init = vs.slice(0, vs.length - 1);
+      const last = vs[vs.length - 1];
+      expect(SingleLinkedList.of(...init).append(last)).toEqual(
+        SingleLinkedList.of(...vs),
+      );
+    }),
+  );
+});
+
+test('appending works the same as with arrays - test case #1', () => {
+  const vs = ['a'];
+  expect(
+    SingleLinkedList.of(...vs.slice(0, vs.length - 1)).append('a'),
+  ).toEqual(SingleLinkedList.of(...vs));
+});
+
+test('basic deletion works', () => {
+  expect(SingleLinkedList.of(1, 2).delete(1)).toEqual(SingleLinkedList.of(2));
+  expect(SingleLinkedList.of(1, 2).delete(3)).toEqual(
+    SingleLinkedList.of(1, 2),
+  );
 });
--- src/SingleLinkedList.ts
+++ src/SingleLinkedList.ts
@@ -1,3 +1,105 @@
-export function add(a: number, b: number): number {
-  return a + b;
+type Node<T> = { tail: null } | { head: T; tail: Node<T> };
+
+// We can share a single empty list between all nodes
+export const EMPTY_NODE: Node<any> = { tail: null };
+
+/**
+ * A SingleLinkedList is a basic data structured, representing a list.
+ *
+ * Each element in the list is linked to the next element. There are no backwards links.
+ */
+export default class SingleLinkedList<T> {
+  constructor(private readonly repr: Node<T>) {}
+
+  /**
+   * Create a new list from an array of values.
+   */
+  static of<T>(...values: T[]): SingleLinkedList<T> {
+    let start: Node<T> = EMPTY_NODE;
+    let end = start;
+    for (const value of values) {
+      const newNode = { head: value, tail: EMPTY_NODE };
+      if (!end.tail) {
+        start = newNode;
+      } else {
+        end.tail = newNode;
+      }
+      end = newNode;
+    }
+    return new SingleLinkedList(start);
+  }
+
+  *[Symbol.iterator](): Iterable<T> {
+    let i = 0;
+    for (let curr = this.repr; curr.tail; curr = curr.tail) {
+      ++i;
+      if (i > 10) {
+        return;
+      }
+      yield curr.head;
+    }
+  }
+
+  /**
+   * Append an element to the end of this list. O(n)
+   */
+  append(t: T): SingleLinkedList<T> {
+    let start: Node<T> = EMPTY_NODE;
+    let end = start;
+
+    const that = this;
+    const iter = function* () {
+      yield* that;
+      yield t;
+    };
+    for (const value of iter()) {
+      const newNode = { head: value, tail: EMPTY_NODE };
+      if (!end.tail) {
+        start = newNode;
+      } else {
+        end.tail = newNode;
+      }
+      end = newNode;
+    }
+    return new SingleLinkedList(start);
+  }
+
+  /**
+   * Add a new element to the front of this list. O(1)
+   */
+  prepend(t: T): SingleLinkedList<T> {
+    return new SingleLinkedList({ head: t, tail: this.repr });
+  }
+
+  /**
+   * Delete an element from this list. O(n)
+   */
+  delete(t: T): SingleLinkedList<T> {
+    let start: Node<T> = EMPTY_NODE;
+    let end = start;
+    let found = false;
+    for (const value of this) {
+      if (!found && value === t) {
+        found = true;
+        continue;
+      }
+      const newNode = { head: value, tail: EMPTY_NODE };
+      if (!end.tail) {
+        start = newNode;
+      } else {
+        end.tail = newNode;
+      }
+      end = newNode;
+    }
+    return new SingleLinkedList(start);
+  }
+
+  /**
+   * Get all the elements of this list, except the first.
+   *
+   * If this list is empty, then this returns an empty list as well.
+   */
+  get tail(): SingleLinkedList<T> {
+    return this.repr.tail ? new SingleLinkedList(this.repr.tail) : this;
+  }
 }
