7056b44fd0e50ae28b2f60dfb00b11ab4ba93994
Lúcás Meier <cronokirby@gmail.com>
2020-04-16 09:19:23 +0200

    Add Head to page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,15 @@
+import Head from 'next/head';
+
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Datastructures | Cronokirby</title>
+        <meta
+          name="description"
+          content="Visualize how data structures work!"
+        />
+      </Head>
       <header>
         <h2 className="text-blue-400">Hello World</h2>
       </header>
